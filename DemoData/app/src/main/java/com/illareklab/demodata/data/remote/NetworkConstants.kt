775d7b1c26e386ca6a572303c40c03c47d08e8b0
Alama Quesada Angelo Aarom <angelo.alama@unmsm.edu.pe>
2026-07-23 08:35:43 -0500

    chore: configurar BASE_URL a produccion tras aprobacion de PR

--- DemoData/app/src/main/java/com/illareklab/demodata/data/remote/NetworkConstants.kt
+++ DemoData/app/src/main/java/com/illareklab/demodata/data/remote/NetworkConstants.kt
@@ -2,7 +2,7 @@ package com.illareklab.demodata.data.remote
 
 object NetworkConstants {
 
-    const val BASE_URL     = "http://10.0.2.2:8000/"
+    const val BASE_URL     = "https://platform-api.kankunapaq.com/"
     const val PROJECT_SLUG = "c22200002"
 
     const val GOOGLE_WEB_CLIENT_ID = "1074854237747-2lh6eg43njj9mvirlqjmv0d4ql2tj53r.apps.googleusercontent.com"
